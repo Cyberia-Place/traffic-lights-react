bb89e6dbcb22f4e50d189202a15723abe45cc115
Cyberia-Place <eliasberto2@gmail.com>
2021-05-03 01:39:25 +0000

    falta arreglar un error

--- src/js/component/lights.js
+++ src/js/component/lights.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Lights = () => {
-	const [color, setColor] = useState("");
+	// const [shineRed, setRed] = useState("");
+	// const [shineYellow, setYellow] = useState("");
+	// const [shineGreen, setGreen] = useState("");
 
 	return (
 		<div>
-			<button className="red-light" onClick=""></button>
-			<button className="yellow-light" onClick=""></button>
-			<button className="green-light" onClick=""></button>
+			<button
+				className="red-light shineRed"
+				// onClick={
+				// 	(setRed("selected"), setYellow(""), setGreen(""))
+				// }
+			></button>
+			<button
+				className="yellow-light shineYellow"
+				// onClick={
+				// 	(setYellow("selected"), setRed(""), setGreen(""))
+				// }
+			></button>
+			<button
+				className="green-light shineGreen"
+				// onClick={
+				// 	(setGreen("selected"), setRed(""), setYellow(""))
+				// }
+			></button>
 		</div>
 	);
 };
