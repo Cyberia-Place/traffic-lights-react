50578ad66a5ab66bcafd060036459664699ad31f
Cyberia-Place <eliasberto2@gmail.com>
2021-05-03 23:38:35 +0000

    terminado

--- src/js/component/lights.js
+++ src/js/component/lights.js
@@ -2,34 +2,24 @@ import React, { useState } from "react";
 
 export const Lights = () => {
 	const [color, setColor] = useState("");
-	// const [shineRed, setRed] = useState("");
-	// const [shineYellow, setYellow] = useState("");
-	// const [shineGreen, setGreen] = useState("");
 
 	return (
 		<div>
-			<button
+			<div
 				onClick={() => setColor("red")}
-				className={`red-light ${color === "red" ? "selected" : ""} `}
-
-				//className="red-light shineRed" onClick = { (setRed("selected"), setYellow(""), setGreen("")) }
-			></button>
-			<button
+				className={`red-light ${
+					color === "red" ? "selected" : ""
+				} `}></div>
+			<div
 				onClick={() => setColor("yellow")}
 				className={`yellow-light ${
 					color === "yellow" ? "selected" : ""
-				} `}
-
-				//className="yellow-light shineYellow" onClick={(setYellow("selected"), setRed(""), setGreen("")) }
-			></button>
-			<button
+				} `}></div>
+			<div
 				onClick={() => setColor("green")}
 				className={`green-light ${
 					color === "green" ? "selected" : ""
-				} `}
-
-				//className="green-light shineGreen" onClick={ (setGreen("selected"), setRed(""), setYellow("")) }
-			></button>
+				} `}></div>
 		</div>
 	);
 };
