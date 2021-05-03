373914b99ff423fd84640048812be72c6593b016
Cyberia-Place <eliasberto2@gmail.com>
2021-05-03 13:08:24 +0000

    casi terminado

--- src/js/component/lights.js
+++ src/js/component/lights.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 export const Lights = () => {
+	const [color, setColor] = useState("");
 	// const [shineRed, setRed] = useState("");
 	// const [shineYellow, setYellow] = useState("");
 	// const [shineGreen, setGreen] = useState("");
@@ -8,22 +9,26 @@ export const Lights = () => {
 	return (
 		<div>
 			<button
-				className="red-light shineRed"
-				// onClick={
-				// 	(setRed("selected"), setYellow(""), setGreen(""))
-				// }
+				onClick={() => setColor("red")}
+				className={`red-light ${color === "red" ? "selected" : ""} `}
+
+				//className="red-light shineRed" onClick = { (setRed("selected"), setYellow(""), setGreen("")) }
 			></button>
 			<button
-				className="yellow-light shineYellow"
-				// onClick={
-				// 	(setYellow("selected"), setRed(""), setGreen(""))
-				// }
+				onClick={() => setColor("yellow")}
+				className={`yellow-light ${
+					color === "yellow" ? "selected" : ""
+				} `}
+
+				//className="yellow-light shineYellow" onClick={(setYellow("selected"), setRed(""), setGreen("")) }
 			></button>
 			<button
-				className="green-light shineGreen"
-				// onClick={
-				// 	(setGreen("selected"), setRed(""), setYellow(""))
-				// }
+				onClick={() => setColor("green")}
+				className={`green-light ${
+					color === "green" ? "selected" : ""
+				} `}
+
+				//className="green-light shineGreen" onClick={ (setGreen("selected"), setRed(""), setYellow("")) }
 			></button>
 		</div>
 	);
